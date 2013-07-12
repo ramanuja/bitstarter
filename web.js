40c55fb3b4b90bf9fda1a4d4a767ac401a648b31
ramanuja <rjosyer@aol.com>
2013-07-12 03:02:26 +0000

    changed web.js

--- web.js
+++ web.js
@@ -3,14 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var buf;
 
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-  buf = data;   
-});
-
 app.get('/', function(request, response) {
-  response.send(JSON.stringify(buf));
+fs.readFile('/index.html', function (err, data) {
+  if (err) throw err;
+  response.writeHead(200, headers);
+  response.end(data);
+  console.log(data);
+   
+});
+return;
+//app.get('/', function(request, response) {
+  //response.send(JSON.stringify(buf));
 });
 
 var port = process.env.PORT || 5000;
