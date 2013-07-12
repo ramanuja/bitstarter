987a0552310021f7f7ee9e6a45e65dc91225ff15
ramanuja <rjosyer@aol.com>
2013-07-12 03:47:47 +0000

    changed web.js

--- web.js
+++ web.js
@@ -4,15 +4,15 @@ var app = express.createServer(express.logger());
 var buf;
 var fs = require('fs');
 app.get('/', function(request, response) {
-/*
-fs.readFile('index.html', function (err, data) {
+var headers = {'Content-type': 'text/html'};
+  fs.readFile('index.html', function (err, data) {
   if (err) {
-  response.writeHead(200, headers);
-  response.end('We encountered an error');
-  return;
+      response.writeHead(200, headers);
+      response.end('We encountered an error');
+      return;
   }  
  
- response.writeHead(200, headers);
+  response.writeHead(200, headers);
   response.end(data);
   console.log(data);
    
@@ -20,7 +20,7 @@ fs.readFile('index.html', function (err, data) {
 return;
 */
 //app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  //response.send(fs.readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
